20b3ec79d2392f887fceb75b8da0a7a0ce80b4a0
kingcos <2821836721v@gmail.com>
2018-04-20 14:53:57 +0800

    Add PathKit dep

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SwiftEagle",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kylef/PathKit", from: "0.9.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftEagle",
-            dependencies: []),
+            dependencies: ["PathKit"]),
     ]
 )
